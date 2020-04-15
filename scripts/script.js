0194ba5a19f5c254a626fa8115e12917c1a7db52
loki87by <61252310+loki87by@users.noreply.github.com>
2020-04-15 17:35:50 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,11 @@ let popup = root.querySelector('.popup');
 const container = popup.querySelector('.popup__container');
 let profileAuthor = profile.querySelector('.profile__title');
 let nameInput = container.querySelector('.popup__text_type_name');
+const profileActivity = profile.querySelector('.profile__subtitle');
+const activityInput = container.querySelector('.popup__text_type_activity');
 function editProfile() {
     popup.classList.add('popup_opened');
-    const profileAuthor = profile.querySelector('.profile__title');
-    const nameInput = container.querySelector('.popup__text_type_name');
     nameInput.value = profileAuthor.textContent;
-    const profileActivity = profile.querySelector('.profile__subtitle');
-    const activityInput = container.querySelector('.popup__text_type_activity');
     activityInput.value = profileActivity.textContent;
 };
 profileButton.addEventListener('click', editProfile);
@@ -26,12 +24,8 @@ closeButton.addEventListener('click', closeEdit);
 const saveButton = container.querySelector('.popup__button');
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    const nameInput = container.querySelector('.popup__text_type_name');
-    const activityInput = container.querySelector('.popup__text_type_activity');
     nameInput.getAttribute('value');
     activityInput.getAttribute('value');
-    const profileAuthor = profile.querySelector('.profile__title');
-    const profileActivity = profile.querySelector('.profile__subtitle');
     profileAuthor.textContent = nameInput.value;
     profileActivity.textContent = activityInput.value;
 }
