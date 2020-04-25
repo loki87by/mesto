4174e36295bd94a8d06d89b45a9ce3644da393aa
loki <loki87.666@gmail.com>
2020-04-26 01:11:39 +0300

    добавить корзину и автокарточки

--- scripts/script.js
+++ scripts/script.js
@@ -31,3 +31,40 @@ function formSubmitHandler (evt) {
 }
 saveButton.addEventListener('click', formSubmitHandler);
 saveButton.addEventListener('click', closeEdit);
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+const sectionCards = document.querySelector('.cards');
+initialCards.forEach(function (item){
+    const cardTemplate = document.querySelector('#card').content;
+    const cardsOnline = document.querySelector('.cards');
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__image').alt = item.name;
+    cardElement.querySelector('.card__title').textContent = item.name;
+    sectionCards.append(cardElement);
+});
+
